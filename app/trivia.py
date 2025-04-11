8e7c64639a4ddc8a139e47a152778d63b624de26
pv4r <pv4r.dd@gmail.com>
2025-04-10 22:01:13 -0500

    Se agrego la clase Quiz

--- app/trivia.py
+++ app/trivia.py
@@ -6,3 +6,18 @@ class Question:
 
     def is_correct(self, answer):
         return self.correct_answer == answer
+
+class Quiz:
+    def __init__(self):
+        self.questions = []
+        self.current_question_index = 0
+
+    def add_question(self, question):
+        self.questions.append(question)
+
+    def get_next_question(self):
+        if self.current_question_index < len(self.questions):
+            question = self.questions[self.current_question_index]
+            self.current_question_index += 1
+            return question
+        return None
